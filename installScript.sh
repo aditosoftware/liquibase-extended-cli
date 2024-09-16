30733f933aa8cb9e710367310fd7eb27181c0720
Ramona Hartinger <r.hartinger@adito.de>
2024-09-16 11:41:18 +0200

    chore: removed not used profile from sh-script

--- installScript.sh
+++ installScript.sh
@@ -14,7 +14,6 @@ jarFile="liquibase-extended-cli.jar"
 # path to the jar file above, from the location of this script.
 jarPath="target/"
 # the path to the folder in which the jar should be moved. Example of vscode-liquibase extension:
-# jarTargetPath="../vscode-liquibase/lib/"
 jarTargetPath="../vscode-liquibase/lib/"
 
 if test -z "$jarFile" || test -z "$jarPath" || test -z "$jarTargetPath"
@@ -26,11 +25,10 @@ else
   jarFilePath=$jarPath$jarFile
   jarFileTargetPath=$jarTargetPath$jarFile
 
-  JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8 JAVA_HOME="C:\Program Files\Java\jdk-13" mvn clean install -T 1C -P adito.m2
+  JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8 JAVA_HOME="C:\Program Files\Java\jdk-13" mvn clean install -T 1C
 
   cp $jarFilePath $jarFileTargetPath
 
   echo "done copying to " $jarFileTargetPath
 
 fi
-
